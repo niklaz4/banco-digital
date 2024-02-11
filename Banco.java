5ad55af67149b74efbea356619f7b15f4ec7e5f2
nicollascal@gmail.com <nicollascal@gmail.com>
2024-02-11 12:26:27 -0300

    banco-digital-java

---
+++ Banco.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class Banco{
+
+    private String nome;
+    private List<Conta> conta;
+    
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(){
+        this.nome = nome;
+    }
+
+    public List<Conta> getConta(){
+        return conta;
+    }
+
+    public void setConta(List<Conta> conta){
+        this.conta = conta;
+    }
+
+}
